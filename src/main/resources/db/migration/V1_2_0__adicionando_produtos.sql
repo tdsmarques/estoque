b5b70982eb3337dbd763f812d54c5ea93d99e5bd
Tiago Marques <ttsmarques@gmail.com>
2020-04-07 05:07:18 -0300

    **

--- src/main/resources/db/migration/V1_2_0__adicionando_produtos.sql
+++ src/main/resources/db/migration/V1_2_0__adicionando_produtos.sql
@@ -1,3 +1,3 @@
-INSERT INTO mysqldb.produto (nome, unidade, quantidade, perecivel)
+INSERT INTO produto (nome, unidade, quantidade, perecivel)
 VALUES ('CANETA BIC', 'UND', '10', 0),
        ('FARINHA DE TRIGO', 'KG', '30', 1);
